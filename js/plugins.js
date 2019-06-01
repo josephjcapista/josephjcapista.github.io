69423ea3465baed2dc4c6ea8533eef1ece68a865
josephjcapista <46794395+josephjcapista@users.noreply.github.com>
2019-06-01 13:31:45 -0400

    Add files via upload
    menu

--- js/plugins.js
+++ js/plugins.js
@@ -22,3 +22,14 @@
 }());
 
 // Place any jQuery/helper plugins in here.
+  $(document).ready(function() {
+
+    // Check for click events on the navbar burger icon
+    $(".navbar-burger").click(function() {
+
+        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+        $(".navbar-burger").toggleClass("is-active");
+        $(".navbar-menu").toggleClass("is-active");
+
+    });
+  });
